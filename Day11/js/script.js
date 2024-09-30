53d9e2029de46b773cc7de89853d11e26bcffbc3
nsanju0413 <nsanju013@gmail.com>
2024-09-30 16:01:09 -0500

    JavaScript

---
+++ Day11/js/script.js
@@ -0,0 +1,14 @@
+// let  a=10;
+// let b=20;
+//
+// let c=a+b;
+//
+// alert("The Value of C is: "+c);
+
+
+const subtotal= 200;
+const taxPercentage = .05;
+const taxAmount= subtotal*taxPercentage;
+let total=subtotal+taxAmount;
+
+console.log("Subtotal = "+subtotal+"\n"+ "Total = "+ total);
