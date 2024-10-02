9d11cc6294dbd99f55ed92bd8c989788cde50e97
nsanju0413 <nsanju013@gmail.com>
2024-10-02 15:57:22 -0500

    Update script.js

---
+++ Day12/js/script.js
@@ -0,0 +1,24 @@
+//Arithmetic functions in Javascript
+
+let sum = 2 + 3;
+console.log("The sum is:", sum);
+
+let difference = 5 - 2;
+console.log("The difference is:", difference);
+
+let product = 4 * 5;
+console.log("The product is:", product);
+
+let quotient = 10 / 2;
+console.log("The quotient is:", quotient);
+
+let remainder = 17 % 5;
+console.log("The remainder is:", remainder);
+
+let result = 2 ** 3;
+console.log("The result is:", result);
+
+let a = 23;
+
+if (a == 23) console.log("true");
+else console.log("false");
