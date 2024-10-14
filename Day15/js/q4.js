f3630bda7c2699b40fafcae7bdeb7426c84adefe
nsanju0413 <nsanju013@gmail.com>
2024-10-14 16:08:03 -0500

    q4

--- Day15/js/q4.js
+++ Day15/js/q4.js
@@ -1,1 +1,15 @@
 //Write a program that gets 5 numbers from a user and prints their max and min.
+
+const num=[];
+
+for(i=0;i<5;i++)
+{
+    num[i]=prompt("Enter a number");
+}
+
+const sorted=num.sort();
+
+console.log("Min is :"+sorted[0])
+console.log("Max is :"+sorted[sorted.length-1])
+
+
