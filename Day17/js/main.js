ad404371e4cbf650f35b163078f8ee0a4caa4fb2
Sanju <nsanju013@gmail.com>
2024-10-23 15:57:45 -0500

    Update main.js

--- Day17/js/main.js
+++ Day17/js/main.js
@@ -1,9 +1,17 @@
-function test(number=5)
-{
-    for(var i=0;i<number;i++)
-    {
-        console.log(i);
-    }
-}
+// function test(number=5)
+// {
+//     for(var i=0;i<number;i++)
+//     {
+//         console.log(i);
+//     }
+// }
+//
+//
+// test();
 
-test();
+
+const getPI=(places=15) =>
+    console.log(Number(Math.PI.toFixed(places)));
+
+const pi=getPI();
+const pi2=getPI(4);
