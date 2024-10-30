eef36ffe764f13a2c0865d53c5d1391519e5447c
Sanjeevlu Buggargani <35345738+nsanju0413@users.noreply.github.com>
2024-10-30 16:12:56 -0500

    practices

---
+++ Day19/js/main.js
@@ -0,0 +1,12 @@
+// const span=document.querySelector("span");
+
+// const bElem=span.firstChild;
+// const bElem=span.firstChild
+
+const h2=document.querySelector("h2");
+
+h2.classList.add("blue");
+
+h2.classList.replace("blue","red");
+
+h2.classList.remove("red");
